46fd8de335d25f3691707ce2433d3a46c6cd40aa
Dirk Dobson <dirkvancouver@live.com>
2018-11-18 13:54:23 -0700

    inial commit

---
+++ csValueTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csValueTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
